575d68627204d8e15910b85e3bb6110cc71ff418
lina <marcecaroc@gmail.com>
2023-06-05 09:00:09 -0500

    feat: create supplies model

---
+++ models/Supplies.py
@@ -0,0 +1,13 @@
+from sqlalchemy import Column, ForeignKey, Integer
+
+from config.database import Base
+
+
+class Supplies(Base):
+
+    __tablename__ ="Supplies"
+
+    id = Column(Integer, primary_key=True)
+    supplier_id = Column(Integer, ForeignKey("supplier.id"))
+    product_id = Column(Integer, ForeignKey("product.id"))
+    purchase_price = Column(Float)

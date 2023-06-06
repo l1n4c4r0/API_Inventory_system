dcf77b021b4af641cff873989cb1191d37017ace
lina <marcecaroc@gmail.com>
2023-06-05 19:07:39 -0500

    dettalles finales

--- main.py
+++ main.py
@@ -4,19 +4,16 @@ from config.database import engine,Base
 
 from middlewares.error_handler import Errorhandler
 
-from routers.movie import movie_router
-from routers.genres import genres_router
+
 from routers.product import product_router
 from routers.supplier import supplier_router
 from routers.supplies import supplies_router
 
 app = FastAPI()
-app.title = "Mi app con FastAPI"
+app.title = "Inventory system FastAPI"
 app.version = "0.0.1"
 
 app.add_middleware(Errorhandler)
-app.include_router(movie_router)
-app.include_router(genres_router)
 app.include_router(product_router)
 app.include_router(supplier_router)
 app.include_router(supplies_router)
